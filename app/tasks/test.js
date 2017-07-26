ea0817cd8944880dd513fc2e1e0e392d2ba90c0b
杨泳樟 <yangyz@mingyuanyun.com>
2017-07-26 20:24:54 +0800

    improve testing feature. command line use "rc test"; add a test sample below seeds floder;

--- app/tasks/test.js
+++ app/tasks/test.js
@@ -9,7 +9,7 @@ process.env.PUBLIC_URL = '';
 // https://github.com/motdotla/dotenv
 require('dotenv').config({silent: true});
 
-const jest = require('jest');
+const jest = require('./jest');
 const argv = process.argv.slice(2);
 
 // Watch unless on CI or in coverage mode
